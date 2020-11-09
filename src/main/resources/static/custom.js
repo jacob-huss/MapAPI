487a66f8645958178f07d60703e4323bb1d28b45
jacob-huss <72151699+jacob-huss@users.noreply.github.com>
2020-11-09 09:09:13 -0600

    keys to git ignore, map controller, css and js ind

---
+++ src/main/resources/static/custom.js
@@ -0,0 +1,13 @@
+var map;
+var coords = {lat: 39.9612, lng: -82.9988};
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: coords,
+        zoom: 10,
+        scrollwheel: false
+    });
+    var marker = new google.maps.Marker({
+        position: coords,
+        map: map
+    });
+}
